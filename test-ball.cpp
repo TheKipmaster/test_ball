c7d69721406f4578d72cb49c46b3bd5280ed9601
TheKipmaster <fkipman@hotmail.com>
2017-04-25 17:37:03 -0300

    -Laço principal de test-ball.cpp refatorado

--- test-ball.cpp
+++ test-ball.cpp
@@ -5,13 +5,17 @@
 
 #include "ball.h"
 
+void run(Simulation & s, double dt) {
+ for (int i = 0; i < 100; ++i) {
+   s.step(dt);
+   s.display();
+ }
+}
+
 int main(int argc, char** argv)
 {
   Ball ball ;
   const double dt = 1.0/30 ;
-  for (int i = 0 ; i < 100 ; ++i) {
-    ball.step(dt) ;
-    ball.display() ;
-  }
+  run(ball, dt);
   return 0 ;
 }
